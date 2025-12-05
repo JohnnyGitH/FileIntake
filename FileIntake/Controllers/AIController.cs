351484a38ae6e125dc96cbeefa68c9cd1ed62728
Johnny Personal <joncockrem@gmail.com>
2025-12-05 09:20:10 -0500

    Updated ViewModel, UI fixes, removed redirect loop

--- FileIntake/Controllers/AIController.cs
+++ FileIntake/Controllers/AIController.cs
@@ -1,29 +1,40 @@
-using FileIntake.Services;
+using System.Threading.Tasks;
+using FileIntake.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FileIntake.Controllers;
 
 public class AIController : Controller
 {
-    private readonly FileIntakeService _fileIntakeService;
+    private readonly IFileIntakeService _fileIntakeService;
 
-    public AIController(FileIntakeService fileIntakeService)
+    public AIController(IFileIntakeService fileIntakeService)
     {
         _fileIntakeService = fileIntakeService;
-        
-    }
+        }
 
-    public IActionResult Index(int? id)
+    public async Task<IActionResult> Index(int? id)
     {
         if(id == null)
         {
             TempData["Error"] = "No file selected for upload.";
-            return RedirectToAction(nameof(Index));
+            return RedirectToAction("Index","FileIntake");
         }
 
         // Get the record to display
-        var file = _fileIntakeService.GetFileByIdAsync(id.Value);
+        var file = await _fileIntakeService.GetFileByIdAsync(id.Value);
 
-        return View();
+        if(file == null || file.FileText?.Length == 0)
+        {
+            TempData["Error"] = "Request file does not exist.";
+            return RedirectToAction("Index","FileIntake");
+        }
+
+        var model = new AiPageViewModel
+        {
+            UploadedFileRecord = file,
+        };
+
+        return View(model);
     }
 }
